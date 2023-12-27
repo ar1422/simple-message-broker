8c3384e09783fa6d2c72e842d4b644a93c9a4b83
aryagrao <ar1422@rit.edu>
2023-12-26 20:01:52 -0500

    Added basic layout of command processing for the client.

---
+++ client/client.go
@@ -0,0 +1,25 @@
+package client
+
+func commandProcessor() {
+	var command = "sample_command"
+	switch command {
+	case get:
+		break
+
+	case put:
+		break
+
+	case subscribe:
+		break
+
+	case unsubscribe:
+		break
+
+	case exit:
+		break
+
+	default:
+		break
+	}
+
+}
---
+++ client/commands.go
@@ -0,0 +1,7 @@
+package client
+
+const exit = "exit"
+const get = "get"
+const put = "put"
+const subscribe = "subscribe"
+const unsubscribe = "unsubscribe"
