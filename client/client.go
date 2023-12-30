3da871c0b765b1fea7669e17ef45674b26fc2d78
aryagrao <ar1422@rit.edu>
2023-12-30 13:38:15 -0500

    Added code for client's commands.

--- client/client.go
+++ client/client.go
@@ -3,17 +3,19 @@ package client
 import (
 	"bufio"
 	"fmt"
+	"message_broker/communication_protocol"
+	"net"
 	"os"
 	"strings"
 )
 
-func extractCommand() string {
+func extractCommand() (string, string) {
 	bufferReader := bufio.NewReader(os.Stdin)
 	var command string
 	command, _ = bufferReader.ReadString('\n')
 	command = strings.TrimSuffix(command, "\n")
-	return strings.Split(command, " ")[0]
-
+	splitValues := strings.SplitN(command, " ", 2)
+	return splitValues[0], splitValues[1]
 }
 
 func userInfoMessages() {
@@ -22,28 +24,79 @@ func userInfoMessages() {
 	fmt.Println("options: get | put <message> | subscribe <topic> | unsubscribe <topic> | exit")
 }
 
-func commandProcessor() {
+func subscriptionHandler(connection net.Conn, topic string) {
+	subscriptionMsg := "subscribe " + topic + "\n"
+	connection.Write([]byte(subscriptionMsg))
+	fmt.Println("Subscribed to the topic - ", topic)
+}
+
+func unsubscriptionHandler(connection net.Conn, topic string) {
+	unsubscriptionMsg := "unsubscribe " + topic + "\n"
+	connection.Write([]byte(unsubscriptionMsg))
+	fmt.Println("Unsubscribed from the topic - ", topic)
+}
+
+func getMessageHandler() string {
+	arguments := communication_protocol.GetMessageArgs{}
+	reply := communication_protocol.GetMessageReply{}
+	if !communication_protocol.Call("Broker.GetMessage", &arguments, &reply) {
+		os.Exit(0)
+	}
+	if reply.Message == "" {
+		return "No messages are available"
+	}
+	return reply.Message
+}
+
+func putMessageHandler(message string) bool {
+	args := communication_protocol.PutBackMessageArgs{Message: message}
+	reply := communication_protocol.PutBackMessageReply{}
+	if !communication_protocol.Call("Broker.PutBackMessage", &args, &reply) {
+		os.Exit(0)
+	}
+	return reply.IsBufferOverflow
+
+}
+
+func listenToUpdates(connection net.Conn) {
+	reader := bufio.NewReader(connection)
+	for {
+		message, _ := reader.ReadString('\n')
+		message = strings.TrimSuffix(message, "\n")
+
+		fmt.Println("<---------------------------------------------------------------------------------------------->")
+		fmt.Println("Message from the topic: ", message)
+		fmt.Println("<---------------------------------------------------------------------------------------------->")
+	}
+}
+
+func commandProcessor(connection net.Conn) {
 	userInfoMessages()
-	var command = extractCommand()
+	var command, arguments = extractCommand()
 
 	switch command {
 	case get:
-		break
+		fmt.Println(getMessageHandler())
 
 	case put:
-		break
+		status := putMessageHandler(arguments)
+		if status {
+			fmt.Println("Ran into error while sending the message")
+		} else {
+			fmt.Println("Message sent successfully.")
+		}
 
 	case subscribe:
-		break
-
+		subscriptionHandler(connection, arguments)
+		go listenToUpdates(connection)
 	case unsubscribe:
-		break
+		unsubscriptionHandler(connection, arguments)
 
 	case exit:
-		break
+		os.Exit(0)
 
 	default:
-		break
+		fmt.Println("Invalid command. Please try again... ")
 	}
 
 }
